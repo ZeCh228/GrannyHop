b7e875005b794dd63822d146034f2651faaabf9d
ZeCh228 <goletiani.david@yandex.ru>
2023-10-09 16:05:44 +0300

    FinishTrigger

---
+++ Assets/Scripts/FinishPoint.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishPoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //1) проверить, что вошёл именно игрок (можно по тегу)
+        //2) если это игрок, то отключаем упр игроком, убираем прыгучесть
+        //3) меню (не делать)
+    }
+
+
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,43 +6,26 @@ using UnityEngine.UIElements;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private Transform SpawnPoint;
-    public Rigidbody rb;
+    [SerializeField] Rigidbody rb;
 
     private void OnCollisionEnter(Collision collision) 
-    {
-        
+    {        
         if (collision.gameObject.CompareTag("Obstacle"))
-        {
-            //Time.timeScale = 0f;
-            StartCoroutine(Wait());
-            
-            //transform.position = SpawnPoint.position;
-            //transform.eulerAngles = Vector3.zero;
-            //Time.timeScale = 1;
-            //rb.velocity = Vector3.zero;
-            //rb.isKinematic = true;           
-            //rb.isKinematic = false;
-            //rb.useGravity = true;
+        {           
+            StartCoroutine(Wait());           
         }
     }
     IEnumerator Wait()
     {        
         Time.timeScale = 0f;
         yield return new WaitForSecondsRealtime(1f);
-        Time.timeScale = 1f;
-        transform.position = SpawnPoint.position;
-        transform.eulerAngles = Vector3.zero;
+        Time.timeScale = 1f;                
+        rb.position = SpawnPoint.position;
+        rb.rotation = Quaternion.Euler(Vector3.zero);
+        rb.velocity = Vector3.zero;
+
         Debug.Log("Stop2sec");        
     }
-
-    /*public IEnumerator Wait() 
-    {
-        //if(gameObject.CompareTag("Obstacle"))
-    yield return new WaitForSeconds(2f);
-
-        transform.position = SpawnPoint.position;
-        transform.eulerAngles = Vector3.zero;
-        Debug.Log("Stop2sec");
-    }*/
+   
 
 }
