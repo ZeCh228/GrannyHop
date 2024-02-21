7939779cb985bc256ac5515916f5d48b35e3fd39
ZeCh <goletiani.david@yandex.ru>
2024-02-21 19:10:51 +0300

    xzc

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,5 +1,7 @@
 ﻿using MoreMountains.Feedbacks;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour 
 {
@@ -11,13 +13,36 @@ public class Portal : MonoBehaviour
     [SerializeField] TimeStars TimeStars;
     [SerializeField] DissolvePortal DissolvePortal;
     [SerializeField] Nasos Nasos;
+    [SerializeField] int CurrentLevel;
+    [SerializeField] int UnlockLevel;
 
+    private void Start()
+    {
+        UnlockLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+    }
 
+    public void LoadLevel(int levelIndex)
+    {
+        if (levelIndex <= UnlockLevel)
+        {
+            SceneManager.LoadScene(levelIndex);
+            CurrentLevel = levelIndex;
+        }
+        else
+        {
+            Debug.Log("Уровень закрыт, пройди прошлый");
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
-        {      
+        {
+            if (CurrentLevel == UnlockLevel && CurrentLevel < 8)
+            {
+                UnlockLevel++;
+                PlayerPrefs.SetInt("UnlockLevel", UnlockLevel);
+            }
              Nasos.enabled = false;
              rb.isKinematic = true;
              Rotate.enabled = false;
