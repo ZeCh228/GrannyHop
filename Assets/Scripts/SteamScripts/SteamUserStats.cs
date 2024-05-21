eedfb953f96e7b172bafb840bd7ccfc328131dc9
pepegas228 <144122122+pepegas228@users.noreply.github.com>
2024-05-21 22:06:56 +0300

    zlaupa

--- Assets/Scripts/SteamScripts/SteamUserStats.cs
+++ Assets/Scripts/SteamScripts/SteamUserStats.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SteamUserStats : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int _currentClicks;
+    private int _oneClick = 1;
+
     void Start()
     {
-        
+        GetUserStat("ClickCount");
+        SetUserStat("ClickCount");
+        SoreUserStat();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SoreUserStat()
     {
-        
+        throw new NotImplementedException();
+    }
+
+    private void SetUserStat(string StatName)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void GetUserStat(string StatName)
+    {
+       bool _isStatRecived = Steamworks.SteamUserStats.GetStat(StatName, out _currentClicks);
+
+        Debug.Log("Recived stat" + _isStatRecived);
+        Debug.Log("Stat data" + _currentClicks);
     }
 }
